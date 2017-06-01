3d6b9e5fe03c9f1918175f3837935f40410e2c5d
Shemamforash <samwest2@hotmai.co.uk>
2017-06-01 18:07:35 +0300

    Project Setup

---
+++ Night Unity Files/Assets/Scripts/Facilitating/UI/ButtonHighlight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	private Text buttonText;
+
+	void Start () {
+		buttonText = transform.FindChild("Text").GetComponent<Text>();
+		Button btn = GetComponent<Button>();
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData){
+		buttonText.color = Color.black;
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		buttonText.color = Color.white;
+	}
+}
