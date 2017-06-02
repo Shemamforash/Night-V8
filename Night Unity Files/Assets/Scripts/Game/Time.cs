a6cd8d5586b91b232a26cfba61f8cfd2cd94f815
Shemamforash <samwest2@hotmai.co.uk>
2017-06-02 21:36:35 +0300

    <Feature> Time and Time Listener
    World time changes in real time.
    World time can be paused.
    Listeners will be notified of pause events, hour change events, and day
    change events.

---
+++ Night Unity Files/Assets/Scripts/Game/Time.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WorldTime : MonoBehaviour
+{
+    private static List<TimeListener> timeListeners = new List<TimeListener>();
+    private static float currentTime, quarterHourTimer = 2.5f;
+    private static int days = 0, hours = 6, minutes = 0;
+    private static bool isNight = false;
+    private static bool isPaused = false;
+
+    public static void SubscribeTimeListener(TimeListener t)
+    {
+        timeListeners.Add(t);
+    }
+
+    private static void BroadcastPause()
+    {
+        foreach (TimeListener t in timeListeners)
+        {
+            t.ReceivePauseEvent(isPaused);
+        }
+    }
+
+    private static void BroadcastHourChange()
+    {
+        foreach (TimeListener t in timeListeners)
+        {
+            t.ReceiveHourEvent();
+        }
+    }
+
+    private static void BroadcastDayChange()
+    {
+        foreach (TimeListener t in timeListeners)
+        {
+            t.ReceiveDayEvent();
+        }
+    }
+
+    public static void Pause()
+    {
+        isPaused = true;
+        BroadcastPause();
+    }
+
+    public static void UnPause()
+    {
+        isPaused = false;
+        BroadcastPause();
+    }
+
+    private void IncrementWorldTime()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= quarterHourTimer)
+        {
+            currentTime -= quarterHourTimer;
+            minutes += 5;
+            if (minutes == 60)
+            {
+                minutes = 0;
+                ++hours;
+                if (hours == 24)
+                {
+                    ++days;
+                }
+                if (hours >= 6 && hours < 20)
+                {
+                    isNight = false;
+                }
+                else
+                {
+                    isNight = true;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!isPaused)
+        {
+            IncrementWorldTime();
+        }
+    }
+}
---
+++ Night Unity Files/Assets/Scripts/Game/TimeListener.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimeListener {
+	public void Subscribe(){
+		WorldTime.SubscribeTimeListener(this);
+	}
+
+	public abstract void ReceiveHourEvent();
+	public abstract void ReceiveDayEvent();
+	public abstract void ReceivePauseEvent(bool paused);
+}
