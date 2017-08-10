fe90f44a84555f0f2b6dcdba370aaa686ddb367a
Shemamforash <samwest2@hotmail.co.uk>
2017-08-10 16:05:23 +0100

    <Feature, Documentation> Started weapon generation
    Created initial classes for weapon generation.
    Decided to do this before starting the combat mechanic, and to be able
    to finish the game UI (i.e. the weapon card and weapon menu).
    Started balancing weapons.
    Created weapon data xml.

---
+++ Night Unity Files/Assets/Scripts/Game/Combat/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game.Combat
+{
+    public class Weapon : ScriptableObject
+    {
+        private float _damage, _accuracy, _fireRate, _handling, _reloadTime, _criticalChance;
+        private int _ammoCapacity;
+        private bool _automatic;
+        private WeaponClass _weaponClass;
+        private WeaponModifier _primaryModifier, _secondaryModifier;
+
+        public Weapon(float damage, float accuracy, float fireRate, float handling, float reloadTime, int ammoCapacity,
+            float criticalChance, bool automatic, WeaponClass weaponClass, WeaponModifier primaryModifier,
+            WeaponModifier secondaryModifier)
+        {
+            _damage = damage;
+            _accuracy = accuracy;
+            _fireRate = fireRate;
+            _handling = handling;
+            _reloadTime = reloadTime;
+            _ammoCapacity = ammoCapacity;
+            _criticalChance = criticalChance;
+            _automatic = automatic;
+            _weaponClass = weaponClass;
+            _primaryModifier = primaryModifier;
+            _secondaryModifier = secondaryModifier;
+        }
+    }
+}
---
+++ Night Unity Files/Assets/Scripts/Game/Combat/WeaponClass.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.Combat
+{
+    public class WeaponClass
+    {
+        
+    }
+}
